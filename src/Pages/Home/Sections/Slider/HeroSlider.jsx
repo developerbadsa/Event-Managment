5b18b3e440f5bf5d76efc80423a0b4e3c47d53e8
Rahim_Badsa <rahimbadsa723@gmail.com>
2023-10-07 11:36:40 +0600

    adding hero slider to home page

--- src/Pages/Home/Sections/Slider/HeroSlider.jsx
+++ src/Pages/Home/Sections/Slider/HeroSlider.jsx
@@ -1,37 +1,41 @@
 import { useState } from "react";
+import { FaRegArrowAltCircleLeft, FaRegArrowAltCircleRight } from "react-icons/fa";
 
 const HeroSlider = ({ images }) => {
-      const [imageIndex, setImageIndex] = useState(0);
+  const [imageIndex, setImageIndex] = useState(0);
 
-      const nextImage = () => {
-            setImageIndex((prevIndex) =>
-                  prevIndex === images.length - 1 ? 0 : prevIndex + 1
-            );
-      };
+  const nextImage = () => {
+    setImageIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
 
-      const prevImage = () => {
-            setImageIndex((prevIndex) =>
-                  prevIndex === 0 ? images.length - 1 : prevIndex - 1
-            );
-      };
+  const prevImage = () => {
+    setImageIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
 
+  const value = images[imageIndex];
+  const bgImg = {
+    background: `url(${value})`, // Corrected background image syntax
+    backgroundSize: "cover", // Optional: Adjust background image properties
+    backgroundRepeat: "no-repeat",
+  };
 
-
-      return (
-            <div className="image-slider">
-                  <button onClick={prevImage} className="text-white">
-                        &lt;fdfdfdfdf
-                  </button>
-                  <img
-                        src={images[imageIndex]}
-                        alt={`Image ${imageIndex + 1}`}
-                        className="w"
-                  />
-                  <button onClick={nextImage} className="slider-button right">
-                        &gt; sdfdfd
-                  </button>
-            </div>
-      );
+  return (
+    <div className="image-slider w-[80%] mx-auto h-screen flex items-center justify-between">
+      <button onClick={prevImage} className="text-white">
+        <FaRegArrowAltCircleLeft></FaRegArrowAltCircleLeft>
+      </button>
+      <div className="p-10" style={bgImg}>
+        {/* Content goes here */}
+      </div>
+      <button onClick={nextImage} className="slider-button right text-white">
+        <FaRegArrowAltCircleRight></FaRegArrowAltCircleRight>
+      </button>
+    </div>
+  );
 };
 
 export default HeroSlider;
