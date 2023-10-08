bdf3c8a801d1e8ca5d2c2561a711953a2b7cec69
Rahim_Badsa <rahimbadsa723@gmail.com>
2023-10-09 00:23:56 +0600

    signup page responsive done desktop mobile and tablet

--- src/Pages/SignUP/Signup.jsx
+++ src/Pages/SignUP/Signup.jsx
@@ -117,13 +117,13 @@ const Signup = () => {
       return (
 
             <div className="flex items-center container mx-auto min-h-[90vh] rounded py-12">
-                  <div className="flex h-full w-full flex-col lg:flex-row justify-between transition-opacity">
-                        <div className="flex flex-col items-center justify-center w-[50%] bg-white text-black rounded-l-lg">
+                  <div className="flex h-full w-full flex-col-reverse lg:flex-row justify-between transition-opacity">
+                        <div className="flex flex-col items-center justify-center w-full lg:w-[50%] bg-white text-black lg:rounded-l-lg py-8">
                               <h1 className="text-5xl font-bold">Sign In</h1>
                               <p className="py-6">Do you have already created acount?</p>
                               <Link to='/signin'><button className="btn hover:bg-red-500 bg-red-500 text-white">Sign In Now</button></Link>
                         </div>
-                        <div className=" flex-1 bg-red-500 py-8 px-10 rounded-r-lg">
+                        <div className=" flex-1 bg-red-500 py-8 px-10 lg:rounded-r-lg">
                               <div>
                                     <h3 className="text-center font-extrabold text-4xl text-red-50 my-8">Sign Up</h3>
                                     <div className="flex gap-4 justify-center my-8">
