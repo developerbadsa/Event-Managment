ffe2596e9269e7a1955c6f9bc4b3fa8c7371fcfb
Rahim_Badsa <rahimbadsa723@gmail.com>
2023-10-09 10:12:11 +0600

    dashboard page made done with dynamic user photo and name email

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,83 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from '../../AuthProvider/AuthProvider';
 
 const Dashboard = () => {
-      return (
-            <div>
-                  <>
-  <button
-    data-popover-target="popover-user-profile"
-    type="button"
-    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-  >
-    User profile
-  </button>
-  <div
-    data-popover=""
-    id="popover-user-profile"
-    role="tooltip"
-    className="absolute z-10 invisible inline-block w-64 text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 dark:text-gray-400 dark:bg-gray-800 dark:border-gray-600"
-  >
-    <div className="p-3">
-      <div className="flex items-center justify-between mb-2">
-        <a href="#">
-          <img
-            className="w-10 h-10 rounded-full"
-            src="/docs/images/people/profile-picture-1.jpg"
-            alt="Jese Leos"
-          />
-        </a>
-        <div>
-          <button
-            type="button"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xs px-3 py-1.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-          >
-            Follow
-          </button>
-        </div>
-      </div>
-      <p className="text-base font-semibold leading-none text-gray-900 dark:text-white">
-        <a href="#">Jese Leos</a>
-      </p>
-      <p className="mb-3 text-sm font-normal">
-        <a href="#" className="hover:underline">
-          @jeseleos
-        </a>
-      </p>
-      <p className="mb-4 text-sm">
-        Open-source contributor. Building{" "}
-        <a
-          href="#"
-          className="text-blue-600 dark:text-blue-500 hover:underline"
-        >
-          flowbite.com
-        </a>
-        .
-      </p>
-      <ul className="flex text-sm">
-        <li className="mr-2">
-          <a href="#" className="hover:underline">
-            <span className="font-semibold text-gray-900 dark:text-white">
-              799
-            </span>
-            <span>Following</span>
-          </a>
-        </li>
-        <li>
-          <a href="#" className="hover:underline">
-            <span className="font-semibold text-gray-900 dark:text-white">
-              3,758
-            </span>
-            <span>Followers</span>
-          </a>
-        </li>
-      </ul>
-    </div>
-    <div data-popper-arrow="" />
-  </div>
-</>
+  const { user } = useContext(UserContext)
 
+  const { photoURL, displayName, email, emailVerified} = user;
+  console.log(displayName)
+
+  return (
+    <div className="p-3 text-white flex justify-between flex-col lg:flex-row max-w-7xl mx-auto my-12">
+          <div className="flex justify-around mb-2 w-[80%] lg:w-1/2 items-center">
+            <img
+              className="w-full rounded-lg"
+              src={photoURL}
+              alt={displayName}
+            />
+            <div>
             </div>
-      );
+          </div>
+          <div className="text-base font-semibold p-10 text-white flex flex-col gap-3">
+           {
+           displayName &&  <div className='text-xl lg:text-3xl font-bold '> <span>Full Name: </span> {displayName}</div>
+           }
+            <div className='h-10 text-base lg:text-lg my-6 '><span>{email}</span>{emailVerified ? <div className="badge badge-secondary mx-2">Verified</div>:  <div className="badge badge-secondary mx-2">Not Verified</div>}</div>
+          </div>
+    </div>
+  );
 };
 
 export default Dashboard;
