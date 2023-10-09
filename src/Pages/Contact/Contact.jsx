2bef54d02de015ac99b6f6a9143a5494449fa29a
Rahim_Badsa <rahimbadsa723@gmail.com>
2023-10-09 11:34:03 +0600

    contact page responsive done

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -3,10 +3,10 @@ import { FaEnvelope, FaLocationArrow, FaPhone, FaVoicemail } from "react-icons/f
 
 const Contact = () => {
       return (
-            <div className="text-white my-16 max-w-7xl mx-auto">
+            <div className="text-white my-16 max-w-7xl px-5 lg:px-1 mx-auto">
                   <div className="text-center">
                         <h2 className="text-3xl font-bold my-4">Countact Us</h2>
-                        <p className="px-64 my-6 text-slate-300 text-base">Welcome to the Hackathon event! We're thrilled to have you on this exciting journey of innovation, collaboration, and creativity. If you have any questions, need assistance, or would like to connect with our team, you've come to the right place.</p>
+                        <p className="lg:px-64 my-6 text-slate-300 text-base">Welcome to the Hackathon event! We're thrilled to have you on this exciting journey of innovation, collaboration, and creativity. If you have any questions, need assistance, or would like to connect with our team, you've come to the right place.</p>
                         <div className="grid grid-cols-1 lg:grid-cols-3 my-12 gap-10">
                               <div className="flex flex-col justify-center shadow-md p-12 bg-slate-700">
                                     <span className="my-3 mx-auto"><FaLocationArrow className="text-2xl text-red-400 text-center"></FaLocationArrow></span>
@@ -26,7 +26,7 @@ const Contact = () => {
                         </div>
                   </div>
                   <div>
-                        <form className="flex flex-col text-black text-center my-16 gap-8" action="">
+                        <form className="flex flex-col text-black text-center px-5 lg:px-1 my-16 gap-8" action="">
                               <h3 className="text-3xl font-bold text-white">React Us With Email</h3>
                               <input type="text" placeholder="Name" className="input input-bordered w-full" required/>
                               <input type="text" placeholder="Email" className="input input-bordered w-full" required/>
