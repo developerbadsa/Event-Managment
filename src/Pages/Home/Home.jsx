914560dedb223ffcb83a8c4049af11293b7853c3
Rahim_Badsa <rahimbadsa723@gmail.com>
2023-10-09 12:29:16 +0600

    aos animation added to home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,9 +3,16 @@ import Navbar from "../../Components/Navbar/Navbar";
 import Conference from "./Sections/Conference/Conference";
 import Services from "./Sections/Services/Services";
 import HeroSlider from "./Sections/Slider/HeroSlider";
+import Aos from "aos";
+import React from "react";
+import 'aos/dist/aos.css';
 
 
 const Home = () => {
+
+      React.useEffect(() => {
+            Aos.init();
+      }, []);
 
       const images = [
             "https://o2oevents.com/wp-content/uploads/2020/06/shutterstock_1723357468-1024x576.png",
@@ -13,17 +20,25 @@ const Home = () => {
       ]
 
       return (
-            <div className="">
+            < div className="" >
                   <div className="max-w-7xl mx-auto">
-                        <HeroSlider images={images}></HeroSlider>
+                        <div data-aos="fade-up">
+                              <HeroSlider images={images}></HeroSlider>
+                        </div>
                         <br />
-                        <AboutUs></AboutUs>
-                        <Services></Services>
-                        <Conference></Conference>
+                        <div data-aos="flip-left" data-aos-delay="100" data-aos-anchor-placement="top-center">
+                              <AboutUs></AboutUs>
+                        </div>
+                        <div data-aos="fade-up" data-aos-delay="1000">
+                              <Services ></Services>
+                        </div>
+                        <div data-aos="flip-right" data-aos-delay="500" data-aos-offset="1000">
+                              <Conference  ></Conference>
+                        </div>
                   </div>
 
-                
-            </div>
+
+            </div >
       );
 };
 
